051052e58ff77ae90d43eed54430749e841dbdfb
Anthony Nguyen <133364236+AFONY10@users.noreply.github.com>
2026-04-10 16:51:25 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,21 @@
+# 1. Hashmap to groupAnagrams (string, list)
+# 2. Append values of hash keys into a return array
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        grpList = {}
+        output = []
+        n = len(strs)
+
+        for i in range(n):
+            key = ''.join(sorted(strs[i]))
+            if key not in grpList:
+                grpList[key] = [strs[i]]
+            else:
+                grpList[key].append(strs[i])
+        
+        for key in grpList:
+            output.append(grpList[key])
+        
+        return output
+
+
