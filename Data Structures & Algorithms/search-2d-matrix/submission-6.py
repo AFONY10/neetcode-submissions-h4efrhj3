bfb3a8ecbe5a242a7d9fa8bae32a5310ef5b5ca0
Anthony Nguyen <133364236+AFONY10@users.noreply.github.com>
2026-04-22 19:18:12 +0200

    Add: search-2d-matrix - submission-6

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-6.py
@@ -0,0 +1,33 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # Find correct row with BS
+        top, bottom = 0, len(matrix) - 1
+        foundRow = []
+        while top <= bottom:
+            middleRow = (top+bottom) // 2
+
+            if target > matrix[middleRow][-1]:
+                top = middleRow + 1
+            elif target < matrix[middleRow][0]:
+                bottom = middleRow - 1
+            else:
+                foundRow = matrix[middleRow]
+                break
+
+        # Find target in foundRow
+        lp = 0
+        rp = len(foundRow) - 1
+
+        while lp <= rp:
+            mp = (rp+lp) // 2
+
+            if target < foundRow[mp]:
+                rp = mp - 1
+            elif target > foundRow[mp]:
+                lp = mp + 1
+            else:
+                return True
+
+        return False
+
+
