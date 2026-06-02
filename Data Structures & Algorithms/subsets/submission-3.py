8f9a097ebde8d5b76800b5eed8b9fd7274ecebe7
Anthony Nguyen <133364236+AFONY10@users.noreply.github.com>
2026-06-02 14:33:09 +0200

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(i, path):
+                if i >= len(nums):
+                        res.append(path.copy())
+                        return
+                
+                currNum = nums[i]
+
+                # If include:
+                path.append(currNum)
+                backtrack(i+1, path)
+                path.pop()
+
+                #if exclude
+                backtrack(i+1, path)
+        
+        backtrack(0, [])
+
+        return res
